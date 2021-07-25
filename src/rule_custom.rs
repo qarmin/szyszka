3eb9e32692250ce176fa7ea1aa43a3cbd8383fc1
Rafał Mikrut <41945903+qarmin@users.noreply.github.com>
2021-07-25 21:46:48 +0200

    New parser of custom rules (#45)

--- src/rule_custom.rs
+++ src/rule_custom.rs
@@ -5,93 +5,138 @@ use humansize::{file_size_opts as options, FileSize};
 use std::cmp::min;
 use std::path::Path;
 
+// enum CustomTypes {
+//     Creation,
+// }
+
 pub fn rule_custom(data_to_change: &str, rule: &SingleRule, rule_number: u64, file_data: Option<(u64, u64, u64)>) -> String {
     let (name, extension) = split_file_name(Path::new(data_to_change));
-    let mut return_string = rule.rule_data.custom_text.clone();
+    let string_to_parse = rule.rule_data.custom_text.clone();
 
     let creation_date: String;
     let modification_date: String;
     let size: String;
 
-    // Random data to visualize typical
+    let mut new_string = "".to_string();
+
+    // Random data to visualize typical usage in examples
     if let Some(f_data) = file_data {
-        modification_date = NaiveDateTime::from_timestamp(f_data.0 as i64, 0).to_string();
-        creation_date = NaiveDateTime::from_timestamp(f_data.1 as i64, 0).to_string();
+        modification_date = NaiveDateTime::from_timestamp(f_data.0 as i64, 0).to_string().replace(":", "_");
+        creation_date = NaiveDateTime::from_timestamp(f_data.1 as i64, 0).to_string().replace(":", "_");
         size = f_data.2.file_size(options::BINARY).unwrap();
     } else {
-        creation_date = "2021-01-31 08:42:12".to_string();
-        modification_date = "2015-11-15 14:24:55".to_string();
+        creation_date = "2021-01-31 08_42_12".to_string();
+        modification_date = "2015-11-15 14_24_55".to_string();
         size = "2 KB".to_string();
     }
 
     match rule.rule_type {
         RuleType::Custom => match rule.rule_place {
             RulePlace::None => {
-                return_string = return_string.replace("$(NAME)", name.as_str());
-                return_string = return_string.replace("$(EXT)", extension.as_str());
-                return_string = return_string.replace("$(SIZE)", size.as_str());
-                return_string = return_string.replace("$(CREAT)", creation_date.as_str());
-                return_string = return_string.replace("$(MODIF)", modification_date.as_str());
-                return_string = return_string.replace("$(CURR)", data_to_change);
+                // let mut start_index: u32 = 0;
+                let mut latest_end_index: usize = 0;
+                loop {
+                    if let Some(start) = string_to_parse[latest_end_index..].find("$(") {
+                        if let Some(end) = string_to_parse[latest_end_index + 2 + start..].find(')') {
+                            new_string.push_str(&string_to_parse[latest_end_index..latest_end_index + start]);
 
-                // Part with numbers - TODO move this to different function
-                if return_string.contains("$(N:") {
-                    let mut index = 0;
-                    while let Some(mut start_index) = return_string[index..].find("$(N:") {
-                        start_index += index + 4;
-                        if let Some(mut first_double) = return_string[start_index..].find(':') {
-                            first_double += start_index;
-                            if let Some(mut second_double) = return_string[first_double + 1..].find(':') {
-                                second_double += first_double + 1;
-                                if let Some(mut end_index) = return_string[second_double..].find(')') {
-                                    end_index += second_double;
+                            let typ = string_to_parse[latest_end_index + start + 2..end + start + latest_end_index + 2].split(':').collect::<Vec<&str>>();
 
-                                    let str_start_number = return_string[start_index..first_double].to_string();
-                                    let str_step_number = return_string[first_double + 1..second_double].to_string();
-                                    let str_fill_zeros = return_string[second_double + 1..end_index].to_string();
+                            let mut invalid_data = true;
+                            match typ[0] {
+                                "CURR" => {
+                                    if typ.len() == 1 {
+                                        new_string.push_str(data_to_change);
+                                        invalid_data = false;
+                                    }
+                                }
+                                "NAME" => {
+                                    if typ.len() == 1 {
+                                        new_string.push_str(&name);
+                                        invalid_data = false;
+                                    }
+                                }
+                                "EXT" => {
+                                    if typ.len() == 1 {
+                                        new_string.push_str(&extension);
+                                        invalid_data = false;
+                                    }
+                                }
+                                "SIZE" => {
+                                    if typ.len() == 1 {
+                                        new_string.push_str(&size);
+                                        invalid_data = false;
+                                    }
+                                }
+                                "CREAT" => {
+                                    if typ.len() == 1 {
+                                        new_string.push_str(&creation_date);
+                                        invalid_data = false;
+                                    }
+                                }
+                                "MODIF" => {
+                                    if typ.len() == 1 {
+                                        new_string.push_str(&modification_date);
+                                        invalid_data = false;
+                                    }
+                                }
+                                "N" => {
+                                    invalid_data = true;
+                                    if typ.len() == 4 {
+                                        if let Ok(start_number) = typ[1].parse::<i64>() {
+                                            if let Ok(step_number) = typ[2].parse::<i64>() {
+                                                if let Ok(fill_zeros) = typ[3].parse::<u64>() {
+                                                    // TODO think about putting it to docs or explaining it somewhere that bigger values will crash entire app, so value must be clamped
+                                                    let fill_zeros = min(fill_zeros, 50);
 
-                                    if let Ok(start_number) = str_start_number.parse::<i64>() {
-                                        if let Ok(step_number) = str_step_number.parse::<i64>() {
-                                            if let Ok(fill_zeros) = str_fill_zeros.parse::<u64>() {
-                                                // TODO think about putting it to docs or explaining it somewhere that bigger values will crash entire app, so value must be clamped
-                                                let fill_zeros = min(fill_zeros, 50);
-
-                                                let mut number;
-                                                if step_number.checked_mul(rule_number as i64).is_none() {
-                                                    number = 0;
-                                                } else {
-                                                    number = step_number * rule_number as i64;
-                                                }
-                                                // TODO Better handle negative numbers
-
-                                                if number.checked_add(start_number).is_none() {
-                                                    number = 0;
-                                                } else {
-                                                    number += start_number;
-                                                }
-
-                                                let mut text_to_replace = number.to_string();
-
-                                                let mut zeros: String = "".to_string();
-                                                if text_to_replace.len() < fill_zeros as usize {
-                                                    for _i in 0..(fill_zeros - text_to_replace.len() as u64) {
-                                                        zeros.push('0');
+                                                    let mut number;
+                                                    if step_number.checked_mul(rule_number as i64).is_none() {
+                                                        number = 0;
+                                                    } else {
+                                                        number = step_number * rule_number as i64;
                                                     }
-                                                    text_to_replace = zeros + text_to_replace.as_str();
+                                                    // TODO Better handle negative numbers
+
+                                                    if number.checked_add(start_number).is_none() {
+                                                        number = 0;
+                                                    } else {
+                                                        number += start_number;
+                                                    }
+
+                                                    let mut text_to_replace = number.to_string();
+
+                                                    let mut zeros: String = "".to_string();
+                                                    if text_to_replace.len() < fill_zeros as usize {
+                                                        for _i in 0..(fill_zeros - text_to_replace.len() as u64) {
+                                                            zeros.push('0');
+                                                        }
+                                                        text_to_replace = zeros + text_to_replace.as_str();
+                                                    }
+
+                                                    new_string.push_str(&text_to_replace);
+
+                                                    invalid_data = false;
                                                 }
-
-                                                return_string = format!("{}{}{}", &return_string[..start_index - 4], text_to_replace, &return_string[end_index + 1..]);
-
-                                                index = start_index - 4 + text_to_replace.len();
-                                                continue;
                                             }
                                         }
                                     }
                                 }
+                                _ => {}
                             }
+
+                            if invalid_data {
+                                new_string.push_str(&string_to_parse[latest_end_index + start..latest_end_index + 2 + start]);
+                                latest_end_index = start + latest_end_index + 2;
+                            } else {
+                                latest_end_index = start + end + 1 + latest_end_index + 2;
+                            }
+                        } else {
+                            new_string.push_str(&string_to_parse[latest_end_index + start..]);
+                            break; // No more )
                         }
-                        // Some things was not parsed successfully, so we need to start 4 positions later(after first "$(N:")
-                        index = start_index;
+                    } else {
+                        new_string.push_str(&string_to_parse[latest_end_index..]);
+                        break; // There is no more things special things
                     }
                 }
             }
@@ -100,7 +145,7 @@ pub fn rule_custom(data_to_change: &str, rule: &SingleRule, rule_number: u64, fi
         _ => panic!("Invalid Rule Type for Custom"),
     }
 
-    return_string
+    new_string
 }
 
 #[cfg(test)]
@@ -122,8 +167,18 @@ mod test {
         write!(file_handler, "50").unwrap();
         file_handler.flush().unwrap();
 
+        rule.rule_data.custom_text = "  )  $(CURR)".to_string();
+        assert_eq!(rule_custom("wombat.txt", &rule, 0, None), "  )  wombat.txt");
+        rule.rule_data.custom_text = "$($(CURR)".to_string();
+        assert_eq!(rule_custom("wombat.txt", &rule, 0, None), "$(wombat.txt");
         rule.rule_data.custom_text = "$(CURR)".to_string();
         assert_eq!(rule_custom("wombat.txt", &rule, 0, None), "wombat.txt");
+        rule.rule_data.custom_text = "$(CURR )".to_string();
+        assert_eq!(rule_custom("wombat.txt", &rule, 0, None), "$(CURR )");
+        rule.rule_data.custom_text = "$(CURR)$(CURR)$(CURR)".to_string();
+        assert_eq!(rule_custom("wombat.txt", &rule, 0, None), "wombat.txtwombat.txtwombat.txt");
+        rule.rule_data.custom_text = "Roman $(CURR) Roman".to_string();
+        assert_eq!(rule_custom("wombat.txt", &rule, 0, None), "Roman wombat.txt Roman");
 
         rule.rule_data.custom_text = "$(NAME)".to_string();
         assert_eq!(rule_custom("wombat.txt", &rule, 0, None), "wombat");
